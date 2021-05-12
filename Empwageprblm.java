80b15d3bd44ad00ef0c5a310fc7f700afe94f942
tech-engg <techsolution.enggn@gmail.com>
2021-05-12 17:40:23 +0530

    Employee Atendence

---
+++ Empwageprblm.java
@@ -0,0 +1,17 @@
+package com.Empwageprblm;
+
+public class Empageprblm {
+
+	public static void main(String[] args) {
+	//conastant
+	int IS_FULL_TIME = 1;
+	double empcheck = Math.floor(Math.random() * 10) % 2;
+	if(empcheck = IS_FULL_TIME);
+	System.out.println("Employee present");
+	else
+	System.out.println("Employee Absent");
+		// TODO Auto-generated method stub
+
+	}
+
+}
