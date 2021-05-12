86c7953de642e0360b938f628de496c4e6f5f139
tech-engg <techsolution.enggn@gmail.com>
2021-05-12 18:01:19 +0530

    added daily wage

--- Empwageprblm.java
+++ Empwageprblm.java
@@ -5,11 +5,18 @@ public class Empageprblm {
 	public static void main(String[] args) {
 	//conastant
 	int IS_FULL_TIME = 1;
+	int EMP_RATE_PER_HOUR = 20;
+	//VARIABLE
+	int empHrs = 0;
+	int empWage = 0;
+	//Commputaion
 	double empcheck = Math.floor(Math.random() * 10) % 2;
-	if(empcheck = IS_FULL_TIME);
-	System.out.println("Employee present");
+	if(empcheck == IS_FULL_TIME);
+	empHrs = 8;
 	else
-	System.out.println("Employee Absent");
+	empHrs = 0;
+	empWage = empHrs * EMP_RATE_PER_HOUR;
+	System.out.println("Emp Wage: " + empWage);
 		// TODO Auto-generated method stub
 
 	}
