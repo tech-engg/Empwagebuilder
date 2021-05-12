dc6087d58a300af00fcfa7da4dea1cf4dd2f4fc3
tech-engg <techsolution.enggn@gmail.com>
2021-05-12 09:22:56 +0530

    fisrst commit

---
+++ Empageprblm.java
@@ -0,0 +1,11 @@
+package com.Empwageprblm;
+
+public class Empageprblm {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to empwage ");
+		// TODO Auto-generated method stub
+
+	}
+
+}
