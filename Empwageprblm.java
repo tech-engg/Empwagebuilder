5942f4018c0e35f7c52466b1e5f53e5b2dc5d651
tech-engg <techsolution.enggn@gmail.com>
2021-05-12 20:23:17 +0530

    added part time

--- Empwageprblm.java
+++ Empwageprblm.java
@@ -2,17 +2,21 @@ package com.Empwageprblm;
 public class Empageprblm {
 	public static void main(String[] args) {
 	  //conastant
-	int IS_FULL_TIME = 1;
+	int IS_FULL_TIME = 2;
 	int EMP_RATE_PER_HOUR = 20;
-	  //VARIABLE
+	int IS_PART_TIME = 1;
+	 
+ //VARIABLE
 	int empHrs = 0;
 	int empWage = 0;
 	  //Commputaion
 	double empcheck = Math.floor(Math.random() * 10) % 2;
 	if(empcheck == IS_FULL_TIME);
+	empHrs = 4;
+	else if (empCheck == IS_PART_TIME)
 	empHrs = 8;
 	else
-	empHrs = 0;
+         empHrs=0;
 	empWage = empHrs * EMP_RATE_PER_HOUR;
 	System.out.println("Emp Wage: " + empWage);
 	}
