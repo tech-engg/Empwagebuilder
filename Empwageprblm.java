c8efa1d594f59bf4c081ebc73f6ca8de3fbe0643
tech-engg <techsolution.enggn@gmail.com>
2021-05-17 18:47:11 +0530

    using switch statement

--- Empwageprblm.java
+++ Empwageprblm.java
@@ -1,22 +1,26 @@
 package com.Empwageprblm;
-public class Empageprblm {
-	public static void main(String[] args) {
+public class Empwageprblm {
 	  //conastant
-	int IS_FULL_TIME = 2;
-	int EMP_RATE_PER_HOUR = 20;
-	int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int IS_PART_TIME = 1;
 	 
  //VARIABLE
+ public static void main(String[] args) {
 	int empHrs = 0;
 	int empWage = 0;
 	  //Commputaion
-	double empcheck = Math.floor(Math.random() * 10) % 2;
-	if(empcheck == IS_FULL_TIME);
-	empHrs = 4;
-	else if (empCheck == IS_PART_TIME)
-	empHrs = 8;
-	else
-         empHrs=0;
+	int empcheck = (int) Math.floor(Math.random() * 10) % 2;
+	switch(empcheck) {
+	  case IS_PART_TIME:
+	      empHrs = 4;
+	      break;
+	  case  IS_FULL_TIME:
+	    empHrs =8;
+	       break;
+	     default:
+	empHrs = 0 ;
+}	
 	empWage = empHrs * EMP_RATE_PER_HOUR;
 	System.out.println("Emp Wage: " + empWage);
 	}
