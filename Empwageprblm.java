8fccc5d4855639974077b60441142233f98f7d48
tech-engg <techsolution.enggn@gmail.com>
2021-05-19 20:08:52 +0530

    added wage for month

--- Empwageprblm.java
+++ Empwageprblm.java
@@ -1,23 +1,31 @@
 package com.Empwageprblm;
+
 public class Empwageprblm {
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int NUM_OF_WORKING_DAYS = 1;
+
+	// VARIABLE
 	public static void main(String[] args) {
-	  //conastant
-	int IS_FULL_TIME = 2;
-	int EMP_RATE_PER_HOUR = 20;
-	int IS_PART_TIME = 1;
-	 
- //VARIABLE
-	int empHrs = 0;
-	int empWage = 0;
-	  //Commputaion
-	double empcheck = Math.floor(Math.random() * 10) % 2;
-	if(empcheck == IS_FULL_TIME)
-	empHrs = 4;
-	else if(empcheck == IS_PART_TIME)
-	empHrs = 8;
-	else
-         empHrs=0;
-	//empWage = empHrs * EMP_RATE_PER_HOUR;
-	System.out.println("Emp Wage: " + empWage);
-	}
+		int empHrs = 0;
+		int empWage = 0;
+		int totalEmpwage=0;
+		// Commputaion
+		int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empcheck) {
+		case IS_PART_TIME:
+			empHrs = 4;
+			break;
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+		default:
+			empHrs = 0;
+		}
+	empWage = empHrs * EMP_RATE_PER_HOUR;
+	totalEmpwage += empWage;
+		System.out.println("Emp Wage: " + empWage);
+	System.out.println("Total Emp Wage:" +totalEmpwage);
+}
 }
