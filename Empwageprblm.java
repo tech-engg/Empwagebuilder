58453a630f370be80ed678a1ccdaade4e55ee6a1
tech-engg <techsolution.enggn@gmail.com>
2021-05-20 07:00:02 +0530

    wage till working days

--- Empwageprblm.java
+++ Empwageprblm.java
@@ -4,14 +4,18 @@ public class Empwageprblm {
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int NUM_OF_WORKING_DAYS = 1;
+	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH =10; 
 
 	// VARIABLE
 	public static void main(String[] args) {
 		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpwage=0;
+		int totalEmpHrs=0;
+		int totalWorkingDays = 0;
 		// Commputaion
+		while(totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+
 		int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 		switch (empcheck) {
 		case IS_PART_TIME:
@@ -23,9 +27,10 @@ public class Empwageprblm {
 		default:
 			empHrs = 0;
 		}
-	empWage = empHrs * EMP_RATE_PER_HOUR;
-	totalEmpwage += empWage;
-		System.out.println("Emp Wage: " + empWage);
-	System.out.println("Total Emp Wage:" +totalEmpwage);
+		totalEmpHrs += empHrs;
+		System.out.println("Days#:" + totalWorkingDays + "Emp Hr:" +empHrs);
+	}
+		int toatalEmpWage =totalEmpHrs * EMP_RATE_PER_HOUR;
+	System.out.println("Total Emp Wage:" +totalEmpHrs);
 }
 }
